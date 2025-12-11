9c18b1a3b852c3a6484aded7a4383c1dfa21f271
vvbocko <wbocko05@gmail.com>
2025-12-11 23:12:53 +0100

    Player input validation (checking format ex. A1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,29 +14,38 @@ public class Main {
             System.out.print("Podaj pole (wzor: A1): ");
 
             String input = scanner.nextLine().trim();
-
             Point p = parsePoint(input, board.getSize());
 
-            gameController.playMove(p);
+            if (p == null) {
+                continue;
+            }
 
+            gameController.playMove(p);
             System.out.println(gameController.getBoardAscii());
         }
     }
     // zamiana małych liter na duże i zamiana podanego A-> 0 , B -> 1 itp aby potem dać to do Point (x,y)
     private static Point parsePoint(String input, int size) {
+        if (input == null || input.isBlank()) {
+            System.out.println("Error: wpisz współrzędne (np. A1)");
+            return null;
+        }
+
         input = input.toUpperCase();
+
+        if (!input.matches("^[A-Z]\\d+$")) {
+            System.out.println("Erorr: zły format (poprawny to np. A1)");
+            return null;
+        }
 
         char colChar = input.charAt(0);
         int x = colChar - 'A';
-
         int y = Integer.parseInt(input.substring(1)) - 1;
 
         if (x < 0 || x >= size || y < 0 || y >= size) {
-            throw new IllegalArgumentException(" pole poza plansza");
+            System.out.println("Error: pole poza planszą");
+            return null;
         }
         return new Point(x, y);
     }
-    // Dodać:
-    // - czy podano poprawny input - długość stringa i czy podano litere jako kolumna
-    // - ogolną obsługe komunikowania błędow uzytkownikowi
 }
