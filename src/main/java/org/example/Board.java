6fa74d11c1521d90a068d43ffbdea00489bccbfb
vvbocko <wbocko05@gmail.com>
2026-01-11 18:18:08 +0100

    JavaDoc comments for Board.java

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -7,32 +7,55 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * Reprezentuje planszę do gry w Go.
+ * Zarządza siatką kamieni, waliduje ruchy zgodnie z zasadami Go
+ * oraz obsługuje mechanikę zbijania kamieni włącznie z wykrywaniem reguły Ko.
+ */
 public class Board {
     private final Stone[][] grid;
     private final int size;
     private Stone[][] previousGrid;
-
     private int lastCapturedCount = 0;
 
+    /**
+     * Tworzy nową planszę Go o określonym rozmiarze (9).
+     *
+     * @param size wymiary planszy
+     */
     public Board(int size) {
         this.grid = new Stone[size][size];
         this.size = size;
         initialiseBoard();
     }
 
+    /**
+     * Inicjalizuje planszę ustawiając wszystkie pozycje jako puste (NONE).
+     */
     private void initialiseBoard() {
-        for (int x = 0; x < size; x++)
-        {
+        for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 grid[x][y] = Stone.NONE;
             }
         }
     }
 
+    /**
+     * Sprawdza czy pozycja na planszy jest pusta.
+     *
+     * @param point pozycja do sprawdzenia
+     * @return true - pozycja jest pusta, false - w przeciwnym razie
+     */
     public boolean isEmpty(Point point) {
         return grid[point.x()][point.y()] == Stone.NONE;
     }
 
+    /**
+     * Zwraca wszystkich sąsiadów danego punktu.
+     *
+     * @param point punkt którego sąsiadów chcemy pobrać
+     * @return lista sąsiednich punktów dla point
+     */
     private List<Point> getNeighbours(Point point) {
         List<Point> neighbourList = new ArrayList<>();
 
@@ -54,9 +77,15 @@ public class Board {
         return neighbourList;
     }
 
+    /**
+     * Znajduje wszystkie kamienie stykające się wspólnie z kamieniem w punkcie startowym.
+     * Wykorzystuje algorytm przeszukiwania Breadth-First Search do znalezienia wszystkich kamieni
+     * tego samego koloru tworzących połączoną grupę.
+     *
+     * @param startPoint punkt startowy
+     * @return lista wszystkich punktów w grupie
+     */
     private List<Point> getGroup(Point startPoint) {
-        // znajdź start point odpowiedniego koloru i od niego przeszukaj getNeighbours()
-        // następnie zapisz points do listy checkedList, i points do listy toBeCheckedList
         List<Point> group = new ArrayList<>();
 
         Stone color = grid[startPoint.x()][startPoint.y()];
@@ -69,33 +98,38 @@ public class Board {
         toBeVisited.add(startPoint);
 
         while(!toBeVisited.isEmpty()) {
-            Point point = toBeVisited.poll(); //poll() - pobiera i usuwa element z kolejki
+            Point point = toBeVisited.poll();
 
-            if (visited[point.x()][point.y()]) { // zabezpieczenie bo ten sam punkt mógł zostać dodany z kilku sąsiadów
+            if (visited[point.x()][point.y()]) {
                 continue;
             }
-            visited[point.x()][point.y()] = true; // zaznaczamy jako odwiedzone i dodajemy do grupy
+            visited[point.x()][point.y()] = true;
             group.add(point);
 
-            for (Point n : getNeighbours(point)) {  //spośrod sąsiadów punktu
+            for (Point n : getNeighbours(point)) {
                 if (visited[n.x()][n.y()]) {
                     continue;
                 }
-                if (grid[n.x()][n.y()] == color) { //sprawdzamy czy ma ten sam kolor co startPoint
+                if (grid[n.x()][n.y()] == color) {
                     toBeVisited.add(n);
                 }
             }
-
         }
         return group;
     }
 
+    /**
+     * Liczy liczbę oddechów (pustych sąsiadujących pól) dla grupy kamieni.
+     *
+     * @param group grupa kamieni dla której liczone są oddechy
+     * @return liczba oddechów grupy
+     */
     private int countBreaths(List<Point> group) {
-        Set<Point> breaths = new HashSet<>(); // biore Set bo jako zbiór bedzie ignorował duplikaty
+        Set<Point> breaths = new HashSet<>();
 
         for (Point points : group) {
             for (Point n : getNeighbours(points)) {
-                if (grid[n.x()][n.y()] == Stone.NONE) { // jak sąsiad jest pusty - dodaj oddech
+                if (grid[n.x()][n.y()] == Stone.NONE) {
                     breaths.add(n);
                 }
             }
@@ -103,13 +137,30 @@ public class Board {
         return breaths.size();
     }
 
+    /**
+     * Usuwa grupę kamieni z planszy.
+     *
+     * @param group grupa kamieni do usunięcia
+     * @return liczba usuniętych kamieni (zdobytych jeńców)
+     */
     private int removeGroup(List<Point> group) {
         for (Point points : group) {
             grid[points.x()][points.y()] = Stone.NONE;
         }
-        return group.size(); // bedzie zwracac liczbe jencow
+        return group.size();
     }
 
+    /**
+     * Próbuje umieścić kamień na planszy w danym punkcie.
+     * Waliduje ruch zgodnie z zasadami:
+     * - Pozycja musi być pusta
+     * - nie można wykonać ruchu samobójczego (chyba, że zbijamy kamienie przeciwnika)
+     * - nie można naruszać reguły Ko (wykonany ruch prowadzi do stanu poprzedniego planszy)
+     *
+     * @param point pozycja gdzie kamień ma być umieszczony
+     * @param stone kolor kamienia do umieszczenia
+     * @return wynik próby wykonania ruchu
+     */
     public MoveResult placeStone(Point point, Stone stone) {
         lastCapturedCount = 0;
 
@@ -118,7 +169,6 @@ public class Board {
         }
 
         Stone[][] snapshotBeforeMove = copyGrid();
-
         grid[point.x()][point.y()] = stone;
         Stone enemyColor = stone.opposite();
 
@@ -137,14 +187,13 @@ public class Board {
         }
 
         for (List<Point> enemiesToKill : enemyGroupToKill) {
-            lastCapturedCount += removeGroup(enemiesToKill); // ile kamieni zostalo zbitych w konkretnym ruchu
+            lastCapturedCount += removeGroup(enemiesToKill);
         }
 
-        List<Point> myGroup = getGroup(point); //do sprawdzenia czy kamien nie wejdzie w "martwe pole"
+        List<Point> myGroup = getGroup(point);
         int myBreaths = countBreaths(myGroup);
 
-        if (myBreaths == 0 && enemyGroupToKill.isEmpty()) // ruch na martwe pole jest okej jeżeli przy tym zabijamy kamienie przeciwnika
-        {
+        if (myBreaths == 0 && enemyGroupToKill.isEmpty()) {
             grid[point.x()][point.y()] = Stone.NONE;
             return MoveResult.SUICIDE;
         }
@@ -162,11 +211,24 @@ public class Board {
         return MoveResult.OK;
     }
 
+    /**
+     * Ręcznie ustawia kamień w danym punkcie.
+     * Używane do synchronizacji planszy w klient-serwer.
+     *
+     * @param p pozycja
+     * @param stone kolor kamienia do umieszczenia
+     */
     public void setStone(Point p, Stone stone){
         grid[p.x()][p.y()] = stone;
     }
 
-
+    /**
+     * Znajduje wszystkie puste grupy połączone z punktem startowym.
+     * Ponowne wykorzystanie algorytmu Breadth-First Search
+     *
+     * @param startPoint startowa pusta pozycja
+     * @return lista wszystkich połączonych pustych pozycji
+     */
     private List<Point> getEmptyArea(Point startPoint) {
         List<Point> area = new ArrayList<>();
 
@@ -195,6 +257,12 @@ public class Board {
         return area;
     }
 
+    /**
+     * Określa, które kolory kamieni otaczają dany pusty obszar.
+     *
+     * @param area pusty obszar do sprawdzenia
+     * @return zbiór kolorów kamieni otaczających obszar
+     */
     private Set<Stone> getBorderColors(List<Point> area) {
         Set<Stone> colors = new HashSet<>();
 
@@ -209,6 +277,12 @@ public class Board {
         return colors;
     }
 
+    /**
+     * Oblicza terytorium graczy.
+     * Terytorium liczone jest jako puste obszary całkowicie otoczone kamieniami jednego koloru.
+     *
+     * @return obiekt TerritoryCount zawieraja liczniki terytorium czarnego i białego
+     */
     public TerritoryCount calculateTerritory() {
         boolean[][] visited = new boolean[size][size];
         int blackTerritory = 0;
@@ -219,7 +293,6 @@ public class Board {
                 Point p = new Point(x, y);
 
                 if (grid[x][y] == Stone.NONE && !visited[x][y]) {
-
                     List<Point> area = getEmptyArea(p);
 
                     for (Point ap : area) {
@@ -242,7 +315,11 @@ public class Board {
         return new TerritoryCount(blackTerritory, whiteTerritory);
     }
 
-
+    /**
+     * Tworzy kopię aktualnego stanu planszy.
+     *
+     * @return kopia stanu planszy
+     */
     private Stone[][] copyGrid() {
         Stone[][] copy = new Stone[size][size];
         for (int x = 0; x < size; x++) {
@@ -253,6 +330,13 @@ public class Board {
         return copy;
     }
 
+    /**
+     * Porównuje dwie siatki planszy pod kątem równości. (do reguły Ko)
+     *
+     * @param a pierwsza siatka
+     * @param b druga siatka
+     * @return true jeśli siatki są identyczne, false w przeciwnym razie
+     */
     private boolean gridsEqual(Stone[][] a, Stone[][] b) {
         if (a == null || b == null) return false;
 
@@ -266,11 +350,19 @@ public class Board {
         return true;
     }
 
+    /**
+     * Czyści stan reguły Ko, pozwalając na wcześniej zabronione ruchy.
+     * Wywoływane gdy gracz pasuje.
+     */
     public void clearKo() {
         previousGrid = null;
     }
 
-
+    /**
+     * Konwertuje planszę na ASCII do wyświetlania w konsoli. (do testów)
+     *
+     * @return tekstowa reprezentacja planszy ze współrzędnymi
+     */
     public String toAscii() {
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -281,13 +373,13 @@ public class Board {
         stringBuilder.append("\n");
 
         for (int y = 0; y < size; y++) {
-            stringBuilder.append(String.format("%2d ", y + 1)); //zwsze szerokie na 2 znaki
+            stringBuilder.append(String.format("%2d ", y + 1));
 
             for (int x = 0; x < size; x++) {
                 Stone color = grid[x][y];
                 String symbol =
                         color == Stone.BLACK ? "B" :
-                        color == Stone.WHITE ? "W" : "+";
+                                color == Stone.WHITE ? "W" : "+";
 
                 stringBuilder.append(String.format("%-3s", symbol));
             }
@@ -297,14 +389,30 @@ public class Board {
         return stringBuilder.toString();
     }
 
+    /**
+     * Zwraca rozmiar planszy.
+     *
+     * @return rozmiar planszy
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Zwraca kolor kamienia w danym punkcie.
+     *
+     * @param p pozycja do sprawdzenia
+     * @return kolor kamienia w tej pozycji
+     */
     public Stone getStone(Point p) {
         return grid[p.x()][p.y()];
     }
 
+    /**
+     * Zwraca liczbę kamieni zbitych w ostatnim ruchu.
+     *
+     * @return liczba zbitych kamieni
+     */
     public int getLastCapturedCount() {
         return lastCapturedCount;
     }
